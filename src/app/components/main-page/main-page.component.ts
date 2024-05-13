14fdc4d627e5f318b30755e3237d9512eb86137b
ateleznikov <s00240505@atu.ie>
2024-05-13 21:54:24 +0100

    Show favourite done

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CryptoTableComponent } from '../crypto-table/crypto-table.component';
+import { FavouriteTableComponent } from '../favourite-table/favourite-table.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CryptoTableComponent],
+  imports: [CryptoTableComponent, FavouriteTableComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
